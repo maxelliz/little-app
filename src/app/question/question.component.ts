115b4f3d06ef44ff415b2b9e3e22d18b297b592f
Maxelliz <batou-du-29@hotmail.fr>
2026-02-10 18:06:58 -0500

    boutons oui grossit non bouge

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -7,15 +7,85 @@ import {Component} from '@angular/core';
   standalone: false
 })
 export class QuestionComponent {
-  protected readonly question = 'Hello World'
 
-  //'Salut Giugiu, est-ce que ça te dit de passer la St-Valentin avec moi ?'
+  protected readonly question = 'Salut Giugiu, est-ce que ça te dit de passer la St-Valentin avec moi ?';
+  boxWidth: any;
+  boxHeight: any;
+  noButtonWidth = 0;
+  noButtonHeight = 0;
+  noButtonLeft = 0;
+  noButtonTop = 0;
+  yesButtonWidth = 0;
+  yesButtonHeight = 0;
+  yesButtonFontSize = 16;
+  minX = 0;
+  maxX = 0;
+  minY = 0;
+  maxY = 0;
+  countClickOnYesButton = 0;
+  maxCountClickOnYesButton = 4;
 
-  answerYes(): void {
-    console.log('YES');
+  ngOnInit() {
+    this.getQuestionBoxWidthHeight();
+    this.getButtonWidthHeight();
+    this.getMinMaxTopLeft();
   }
 
-  answerNo(): void {
-    console.log('NO');
+  getQuestionBoxWidthHeight() {
+    const element = document.querySelector('.question-card-frame') as HTMLElement;
+
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      this.boxWidth = rect.width;
+      this.boxHeight = rect.height;
+    } else {
+      console.error('Element with class "question-card-frame" not found.');
+    }
   }
+
+  getButtonWidthHeight() {
+    const element = document.querySelector('.no-button') as HTMLElement;
+
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      this.noButtonWidth = rect.width;
+      this.noButtonHeight = rect.height;
+      this.yesButtonWidth = rect.width;
+      this.yesButtonHeight = rect.height;
+    } else {
+      console.error('Element with class "no-button" not found.');
+    }
+  }
+
+  getMinMaxTopLeft() {
+    this.minX = -this.boxWidth / 2;
+    this.maxX = (this.boxWidth / 2) - 10 - this.noButtonWidth; //10px margin
+    this.minY = -this.boxHeight / 2;
+    this.maxY = (this.boxHeight / 2) - this.noButtonHeight; //10px margin
+  }
+
+  moveNoButton(): void {
+    const element = document.querySelector('.no-button') as HTMLElement;
+
+    if (element) {
+      this.noButtonLeft = Math.random() * (this.maxX - this.minX) + this.minX;
+      this.noButtonTop = Math.random() * (this.maxY - this.minY) + this.minY;
+    }
+  }
+
+  biggerYesButton(): void {
+    if (this.countClickOnYesButton < this.maxCountClickOnYesButton) {
+      const element = document.querySelector('.yes-button') as HTMLElement;
+
+      if (element) {
+        this.countClickOnYesButton++;
+        this.yesButtonWidth = this.yesButtonWidth * 1.5;
+        this.yesButtonHeight = this.yesButtonHeight * 1.5;
+        this.yesButtonFontSize = this.yesButtonFontSize * 1.5;
+      }
+    } else {
+      // aller a la page de fin
+    }
+  }
+
 }
